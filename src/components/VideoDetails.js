87045370e0a1b98f6420e830530770ac774871f1
efriedla <efriedla20852@gmail.com>
2018-01-31 15:15:01 -0800

    added bootstrap layout for VideoDetails

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const VideoDetails = () => {
+<div className="video-detail col-md-8">
+    <div className="embed-responsive embed-responsive-21by9">
+        <iframe class="embed-responsive-item" src="..."></iframe>
+    </div>
+    <div className="details">
+        <div></div>
+        <div></div>
+    </div>
 
+
+</div>
 }
 
 export default VideoDetails
