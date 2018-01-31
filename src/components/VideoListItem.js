4b6ce8f13db4330bb80842a55d2ccbe0dfc951db
efriedla <efriedla20852@gmail.com>
2018-01-31 15:02:01 -0800

    added image and title

--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -3,16 +3,19 @@ import React from 'react';
 const VideoListItem = ({video}) => {
     // const VideoListItem = (props) => {
     // const video = props.video;
+    const imgURL = video.snippet.thumbnails.default.url; 
+    const title = video.snippet.title;   
     console.log(video.snippet);
     return (
         <li className="list-group-item">
             <div className="video-list media">
                 <div className="media-left">
-                    <img className="media-object" />
+                    <img className="media-object" src={imgURL} />
                 </div>
-            </div>
-            <div className="media-body">
-                <div className="media-heading"></div>
+            
+                <div className="media-body">
+                    <div className="media-heading">{title}</div>
+                </div>
             </div>
         </li>
     );
