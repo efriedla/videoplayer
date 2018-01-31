104a66831bffcf139fd1b3a705158f64366b9df4
efriedla <efriedla20852@gmail.com>
2018-01-31 12:56:07 -0800

    first commit

---
+++ src/components/search_bar.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = { term: "" };
+    }
+    render(){
+        return (
+       
+           <div>
+                <input onChange={event => this.setState({ term: event.target.value})} />
+                <p>Value : {this.state.term}</p>
+           </div>
+        );
+    }
+    // onInputChange(event){
+    //     console.log(event.target.value);
+    // }
+};
+
+export default SearchBar
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import SearchBar from './components/search_bar';
+// import API_KEY from 'config';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+// var mykey = config.MY_KEY;
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
+    
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
